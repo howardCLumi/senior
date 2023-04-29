cfef1908b48555c69e796467baab45489a243368
howardCLumi <102765211+howardCLumi@users.noreply.github.com>
2023-04-29 15:32:06 -0500

    photo added

--- src/pages/StudentView.js
+++ src/pages/StudentView.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./StudentView.css";
 import { Typography } from "@mui/material";
 import "charts.css";
+import graph from "../assets/graph.png";
 
 const StudentView = () => {
   return (
@@ -11,39 +12,9 @@ const StudentView = () => {
           <Typography>filters</Typography>
         </div>
         <div className="student__graph">
-          <table class="chart-css bar">
-            <caption> 2016 Summer Olympics Medal Table </caption>
-
-            <thead>
-              <tr>
-                <th scope="col"> Country </th>
-                <th scope="col"> Gold </th>
-                <th scope="col"> Silver </th>
-                <th scope="col"> Bronze </th>
-              </tr>
-            </thead>
-
-            <tbody>
-              <tr>
-                <th scope="row"> USA </th>
-                <td> 46 </td>
-                <td> 37 </td>
-                <td> 38 </td>
-              </tr>
-              <tr>
-                <th scope="row"> GBR </th>
-                <td> 27 </td>
-                <td> 23 </td>
-                <td> 17 </td>
-              </tr>
-              <tr>
-                <th scope="row"> CHN </th>
-                <td> 26 </td>
-                <td> 18 </td>
-                <td> 26 </td>
-              </tr>
-            </tbody>
-          </table>
+          <div>
+            <img src={graph} classname="graph__photo" alt=""></img>
+          </div>
         </div>
       </div>
     </>
