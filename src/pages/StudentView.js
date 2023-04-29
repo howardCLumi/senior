a5c704f871db68e7b891fbe3030ffa6ac7286d85
Hailee Leonard <hleonard@iastate.edu>
2023-04-29 15:48:43 -0500

    graph

--- src/pages/StudentView.js
+++ src/pages/StudentView.js
@@ -1,22 +1,15 @@
 import React from "react";
 import "./StudentView.css";
 import { Typography } from "@mui/material";
-import "charts.css";
 import graph from "../assets/graph.png";
 
 const StudentView = () => {
   return (
     <>
-      <div className="student__container">
-        <div className="student__filter">
-          <Typography>filters</Typography>
-        </div>
+        <Typography variant={'h6'}>Journey Map</Typography>
         <div className="student__graph">
-          <div>
-            <img src={graph} classname="graph__photo" alt=""></img>
-          </div>
+            <img src={graph} classname="graph__photo" alt=""/>
         </div>
-      </div>
     </>
   );
 };
